858442589e62162bcb68c3ae2e2c9935d29d4adb
rjp1 <46408746+4N24@users.noreply.github.com>
2020-03-01 05:59:45 -0600

    added basic view

--- checkbook-checkout/src/react_components/view.js
+++ checkbook-checkout/src/react_components/view.js
@@ -1,5 +1,23 @@
 import react from 'react';
 import reactDom from 'react-dom';
-
+import Statuscard from 'statusCard.js'
 import { makeStyles } from '@material-ui/core/styles';
- 
+import { createBrotliCompress } from 'zlib';
+import Appbar from 'Appbar.js'
+
+const app = makeStyles(theme=> ({
+background: 'white',
+
+}));
+
+export default function View(){
+    return(
+        <div> 
+            <Appbar></Appbar>
+            <Statuscard></Statuscard>
+    
+        </div>
+   );
+}
+
+
