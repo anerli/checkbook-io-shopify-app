7e9b318adaf7890b37fcc43ab604a6767c70ac78
BuildTools <unconfigured@null.spigotmc.org>
2020-02-29 23:32:12 -0600

    right params

--- checkbook-checkout/src/App.js
+++ checkbook-checkout/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  useLocation
+} from "react-router-dom";
 
-function App() {
+// React Router does not have any opinions about
+// how you should parse URL query strings.
+//
+// If you use simple key=value query strings and
+// you do not need to support IE 11, you can use
+// the browser's built-in URLSearchParams API.
+//
+// If your query strings contain array or object
+// syntax, you'll probably need to bring your own
+// query parsing function.
+
+export default function QueryParamsExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <QueryParamsDemo />
+    </Router>
   );
 }
 
-export default App;
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function QueryParamsDemo() {
+  let query = useQuery();
+
+  return (
+    <div>
+      <h1>Email: {query.get("email")}</h1>
+      <h1>Cost: {query.get("cost")}</h1>
+    </div>
+    /*
+    <div>
+      <div>
+        <h2>Accounts</h2>
+        <ul>
+          <li>
+            <Link to="/account?name=netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/account?name=zillow-group">Zillow Group</Link>
+          </li>
+          <li>
+            <Link to="/account?name=yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/account?name=modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
+        <Child name={query.get("name")} />
+      </div>
+    </div>
+    */
+  );
+}
+
+function Child({ name }) {
+  return (
+    <div>
+      {name ? (
+        <h3>
+          The <code>name</code> in the query string is &quot;{name}
+          &quot;
+        </h3>
+      ) : (
+        <h3>There is no name in the query string</h3>
+      )}
+    </div>
+  );
+}
